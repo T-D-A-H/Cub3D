9273233ab30b7f4210ba8534b4456790e8fe807d
Ja1m3st <jaimesanchezguinea@gmail.com>
2025-03-13 12:18:13 +0100

    struct

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jaimesan <jaimesan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:24:22 by jaimesan          #+#    #+#             */
-/*   Updated: 2025/03/13 11:26:24 by jaimesan         ###   ########.fr       */
+/*   Updated: 2025/03/13 12:17:42 by jaimesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,17 @@
 
 # define ERROR_EXTENSION printf("Error: The file must have a .cub extension\n")
 # define ERROR_FILES printf("Error: ./cub3D  <file.cub>\n")
+
+typedef struct s_cub
+{
+	char *no_path;
+	char *so_path;
+	char *we_path;
+	char *ea_path;
+	int f_color[3];
+	int c_color[3];
+	char **map;
+}	t_cub;
 
 // Main
 int main(int argc, char **argv);
--- src/main/main.c
+++ src/main/main.c
@@ -6,16 +6,23 @@
 /*   By: jaimesan <jaimesan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:08:04 by jaimesan          #+#    #+#             */
-/*   Updated: 2025/03/13 11:35:34 by jaimesan         ###   ########.fr       */
+/*   Updated: 2025/03/13 12:17:31 by jaimesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+/* int	init_structs(argv)
+{
+
+} */
+
 int	main(int argc, char **argv)
 {
 	if (check_cub_args(argc, argv))
 		return (0);
+/* 	if (init_structs(argv)) */
+	
 	printf("Bien\n");
 	return (0);
 }
